8dcaf2bfef09a27b761806748e7f3671dcc08bcb
dorisanse <huyenanhbui021021@gmail.com>
2025-02-21 21:58:44 +0700

    demo component scanning

---
+++ src/main/java/com/spring/spring_component_scanning/SpringComponentScanningApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_component_scanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComponentScanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComponentScanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spring_component_scanning/SpringComponentScanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_component_scanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringComponentScanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
