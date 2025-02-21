cc363ce4b94c4a7539c2087f76a91d1b6f69a6df
dorisanse <huyenanhbui021021@gmail.com>
2025-02-21 21:12:53 +0700

    demo component scanning

--- src/main/java/com/spring/rest/DemoController.java
+++ src/main/java/com/spring/rest/DemoController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach coach;
+
     @Autowired
     public DemoController(Coach coach) {
         this.coach = coach;
     }
+
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
