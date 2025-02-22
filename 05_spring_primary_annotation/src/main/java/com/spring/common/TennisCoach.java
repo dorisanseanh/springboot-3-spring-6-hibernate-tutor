d5d9513fc8df5ab5440c9293440980769f8a13d2
dorisanse <huyenanhbui021021@gmail.com>
2025-02-22 21:34:12 +0700

    demo primary annotation

---
+++ 05_spring_primary_annotation/src/main/java/com/spring/common/TennisCoach.java
@@ -0,0 +1,14 @@
+package com.spring.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Let practice your backhand volley";
+    }
+}
---
+++ 05_spring_primary_annotation/src/main/java/com/spring/common/TrackCoach.java
@@ -0,0 +1,13 @@
+package com.spring.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run coach workout";
+    }
+}
