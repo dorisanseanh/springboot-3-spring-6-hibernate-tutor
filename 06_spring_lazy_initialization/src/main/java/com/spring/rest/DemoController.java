615ddab9d941092eaa4741819a5b15d55db6d1bc
dorisanse <huyenanhbui021021@gmail.com>
2025-02-23 18:08:45 +0700

    demo lazy annotation

---
+++ 06_spring_lazy_initialization/src/main/java/com/spring/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.spring.rest;
+
+import com.spring.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+        System.out.println("In constructor" + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String dailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
