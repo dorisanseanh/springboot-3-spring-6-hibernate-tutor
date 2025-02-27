fbe26c54ba5d24279ee636cbd08517c858ee7956
dorisanse <huyenanhbui021021@gmail.com>
2025-02-28 00:58:55 +0700

    saving a Java Object with JPA coding p1

---
+++ 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.spring.dao;
+
+import com.spring.entity.Student;
+
+public interface StudentDAO {
+    void save (Student student);
+}
---
+++ 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/dao/impl/StudentDAOImpl.java
@@ -0,0 +1,22 @@
+package com.spring.dao.impl;
+
+import com.spring.dao.StudentDAO;
+import com.spring.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+public class StudentDAOImpl implements StudentDAO {
+    //    define field entity manager
+    private EntityManager entiytManager;
+
+    public StudentDAOImpl(EntityManager entiytManager) {
+        this.entiytManager = entiytManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entiytManager.persist(student);
+
+    }
+}
