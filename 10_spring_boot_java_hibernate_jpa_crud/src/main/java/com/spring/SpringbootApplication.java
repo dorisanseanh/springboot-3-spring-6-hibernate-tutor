d97e4fb0512c8e32506d0eab670f915ea356e57f
dorisanse <huyenanhbui021021@gmail.com>
2025-02-28 22:35:36 +0700

    demo add multiple student

--- 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/SpringbootApplication.java
+++ 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/SpringbootApplication.java
@@ -19,19 +19,37 @@ public class SpringbootApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            createStudent(studentDAO);
+            multipleStudent(studentDAO);
+//
         };
     }
 
-    private void createStudent(StudentDAO studentDAO) {
+    private void multipleStudent(StudentDAO studentDAO) {
 //        create the student object
         System.out.println("Creating student");
-        Student student = new Student("Dorisanse", "Bui", "dorisanseanh@gmail.com");
+        Student student1 = new Student("Botina", "Nguyen", "jadonnguyenh@gmail.com");
+        Student student2 = new Student("Ashlenguci", "Nguyen", "ashleynguyen@gmail.com");
+        Student student3 = new Student("Hanna", "Bui", "anna@gmail.com");
 //        save the student
         System.out.println("Saving student: ");
-        studentDAO.save(student);
+        studentDAO.save(student1);
+        studentDAO.save(student2);
+        studentDAO.save(student3);
 //        display id of the saved student
-        System.out.println("Saved student: " + student.getFirstName());
-
+        System.out.println("Saved student: " + student1.getFirstName());
+        System.out.println("Saved student: " + student2.getFirstName());
+        System.out.println("Saved student: " + student3.getFirstName());
     }
+
+//    private void createStudent(StudentDAO studentDAO) {
+////        create the student object
+//        System.out.println("Creating student");
+//        Student student = new Student("Dorisanse", "Bui", "dorisanseanh@gmail.com");
+////        save the student
+//        System.out.println("Saving student: ");
+//        studentDAO.save(student);
+////        display id of the saved student
+//        System.out.println("Saved student: " + student.getFirstName());
+//
+//    }
 }
