f992a96a8563dcbe7baead8a1b92ba2082c2fcf5
dorisanse <huyenanhbui021021@gmail.com>
2025-02-28 21:17:31 +0700

    demo save student with jpa

--- 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/SpringbootApplication.java
+++ 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/SpringbootApplication.java
@@ -1,5 +1,8 @@
 package com.spring;
 
+import com.spring.dao.StudentDAO;
+import com.spring.entity.Student;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,10 +15,23 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
-@Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Welcome to Spring Boot Application");
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
         };
-}
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+//        create the student object
+        System.out.println("Creating student");
+        Student student = new Student("Dorisanse", "Bui", "dorisanseanh@gmail.com");
+//        save the student
+        System.out.println("Saving student: ");
+        studentDAO.save(student);
+//        display id of the saved student
+        System.out.println("Saved student: " + student.getFirstName());
+
+    }
 }
--- 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/dao/impl/StudentDAOImpl.java
+++ 10_spring_boot_java_hibernate_jpa_crud/src/main/java/com/spring/dao/impl/StudentDAOImpl.java
@@ -4,7 +4,9 @@ import com.spring.dao.StudentDAO;
 import com.spring.entity.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class StudentDAOImpl implements StudentDAO {
     //    define field entity manager
     private EntityManager entiytManager;
