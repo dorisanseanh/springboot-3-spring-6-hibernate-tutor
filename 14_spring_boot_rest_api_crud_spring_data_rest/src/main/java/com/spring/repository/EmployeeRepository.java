1c8c2b90d9ca09a814527a29d1da66b738d620ab
dorisanse <huyenanhbui021021@gmail.com>
2025-03-04 22:31:24 +0700

    [Demo] Implement REST API with pagination and sorting

--- 14_spring_boot_rest_api_crud_spring_data_rest/src/main/java/com/spring/repository/EmployeeRepository.java
+++ 14_spring_boot_rest_api_crud_spring_data_rest/src/main/java/com/spring/repository/EmployeeRepository.java
@@ -3,7 +3,11 @@ package com.spring.repository;
 
 import com.spring.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+//@RepositoryRestResource: is an annotation in Spring Data REST that automatically exposes RESTful endpoints
+// for your repository — without needing to write a controller manually.
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
