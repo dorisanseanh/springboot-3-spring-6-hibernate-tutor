afb59e3691c340fbd1d946c8be08cdc451201fb3
dorisanse <huyenanhbui021021@gmail.com>
2025-03-10 22:24:54 +0700

    demo hello world form model

--- 16_spring_boot_thymeleaf/src/main/java/com/spring/thymeleaf/controller/DemoController.java
+++ 16_spring_boot_thymeleaf/src/main/java/com/spring/thymeleaf/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.spring.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DemoController {
@@ -10,5 +11,11 @@ public class DemoController {
     public String demo(Model model) {
         model.addAttribute("message", java.time.LocalDate.now());
         return "demo";
+
+    }
+
+    @PostMapping("/processForm")
+    public String submit() {
+        return "form";
     }
 }
