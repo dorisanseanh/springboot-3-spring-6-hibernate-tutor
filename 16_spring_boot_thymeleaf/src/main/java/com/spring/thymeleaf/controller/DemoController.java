227b6330801424ec8f8f7e88912c6ceb791ce3fe
dorisanse <huyenanhbui021021@gmail.com>
2025-03-10 23:04:20 +0700

    demo Adding Data to Spring MVC Model

--- 16_spring_boot_thymeleaf/src/main/java/com/spring/thymeleaf/controller/DemoController.java
+++ 16_spring_boot_thymeleaf/src/main/java/com/spring/thymeleaf/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.spring.thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,17 @@ public class DemoController {
 
     @PostMapping("/processForm")
     public String submit() {
+        return "form";
+    }
+    @PostMapping("/process")
+    public String process(HttpServletRequest request, Model model) {
+//        read the request parameter from the HTML form
+        String theName = request.getParameter("name");
+//        convert the data to all caps
+        theName = theName.toUpperCase();
+//        create the messagse
+        String result ="Yo! " + theName;
+        model.addAttribute("message", result);
         return "form";
     }
 }
