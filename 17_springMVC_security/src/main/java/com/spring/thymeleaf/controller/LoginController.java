e620e63ad5c104794dffc5bdb95dd4d781ca852c
dorisanse <huyenanhbui021021@gmail.com>
2025-03-25 21:29:27 +0700

    login form message error

--- 17_springMVC_security/src/main/java/com/spring/thymeleaf/controller/LoginController.java
+++ 17_springMVC_security/src/main/java/com/spring/thymeleaf/controller/LoginController.java
@@ -1,2 +1,12 @@
-package com.spring.thymeleaf.controller;public class LoginController {
+package com.spring.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLpginPage() {
+        return "plain-login";
+    }
 }
--- 17_springMVC_security/src/main/java/com/spring/thymeleaf/security/SecurityConfig.java
+++ 17_springMVC_security/src/main/java/com/spring/thymeleaf/security/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.spring.thymeleaf.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +29,20 @@ public class SecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
     }
 }
